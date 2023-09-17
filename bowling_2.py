c606ed3155ff48b9f3eb28637e13354b555538ab
Juha-Matti Santala <juha-matti.santala@madbooster.com>
2023-09-17 11:57:41 +0200

    yay

--- bowling_2.py
+++ bowling_2.py
@@ -3,6 +3,9 @@ import re
 import sys
 from itertools import pairwise
 
+def get_frames(scores):
+  return zip(scores[::2], scores[1::2])
+
 def pprint(*args):
   print(*args, file=sys.stderr)
 
@@ -10,7 +13,13 @@ def parse_bowling_input(input):
   players = []
   player_inputs = input.split('\n')
   for player in player_inputs:
-    scores = [int(s) for s in re.findall('-?\d+', player)]
+    frame_pattern = '(10|\d+ \d+)'
+    frames = re.findall(frame_pattern, player)
+    for i, frame in enumerate(frames):
+      if frame == '10':
+        frames[i] = '10 0'
+    improved_scores = ' '.join(frames)
+    scores = [int(s) for s in re.findall('-?\d+', improved_scores)]
     if invalid_scores := [str(score) for score in scores if score < 0 or score > 10]:
         raise ValueError(f'{", ".join(invalid_scores)} is invalid score')
     if scores:
@@ -34,30 +43,20 @@ def bowling(input):
   ('Juhis', 43)
   >>> bowling('Juhis 0 10 4 5')
   ('Juhis', 24)
+  >>> bowling('Juhis')
+  ('Juhis', 0)
   """
   winner = (None, -1)
   players = parse_bowling_input(input)
   for name, scores in players:
-    pprint(name, scores)
     strikes = 0
     spares = 0
-    first_throw = True
-    frame_score = 0
-    for score in scores:
-      if first_throw:
-        if score == 10:
-          strikes += 1
-        else:
-          frame_score += score
-          first_throw = False
-      else:
-        frame_score += score
-        if frame_score == 10:
-          spares += 1
-        first_throw = True
-        frame_score = 0
+    for first, second in get_frames(scores):
+      if first == 10:
+        strikes += 1
+      elif first + second == 10:
+        spares += 1
 
-    pprint(strikes, spares)
     total_score = sum(scores) if scores else 0
     total_score += 10 * strikes + 5 * spares
     if total_score > winner[1]:
